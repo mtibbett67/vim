66ab35ab7b0df3bb2d6c5a66371ceb8796ea9f2d
Mark Tibbett <mark@Linux>
2017-03-13 21:18:05 -0600

    Updated setup file to include plugins

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
 #! /bin/bash
 cp ./vimrc ~/.vimrc
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle/
+git clone https://github.com/tpope/vim-commentary.git
+git clone https://github.com/tpope/vim-repeat.git
+git clone https://github.com/tpope/vim-surround.git
