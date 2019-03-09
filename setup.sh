db115584498ae08f74910d3e58ad31bfecae8e55
mtibbett67 <mtibbett67@gmail.com>
2019-03-08 19:37:15 -0700

    Update setup.sh
    Switched from using pathogen to apt-vim to manage plugins
    https://github.com/egalpin/apt-vim

--- setup.sh
+++ setup.sh
@@ -3,8 +3,3 @@ cp ./vimrc ~/.vimrc
 mkdir ~/.vim/templates
 cp -r ./templates ~/.vim/
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-cd ~/.vim/bundle/
-git clone https://github.com/tpope/vim-commentary.git
-git clone https://github.com/tpope/vim-repeat.git
-git clone https://github.com/tpope/vim-surround.git
